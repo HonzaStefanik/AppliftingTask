3c5e84e79bbf51acd8d93b6fe89dd0e6adaa5f40
Jan Štefánik <jan.stefanik@nangu.tv>
2020-08-17 15:02:48 +0200

    Initial commit

---
+++ src/main/java/applifting/task/AppliftingTaskApplication.java
@@ -0,0 +1,13 @@
+package applifting.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliftingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliftingTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/applifting/task/AppliftingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package applifting.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliftingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
